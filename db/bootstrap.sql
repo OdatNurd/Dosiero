6a126174f96d267723fc7c3b8df0500352cce54e
Terence Martin <odatnurd@gmail.com>
2024-10-16 10:43:46 -0500

    Include some initial DB design and schema files
    We are working with dbdiagram.io as the design tool of choice for this
    project, so included in the repository is now the current DBML file
    that describes the database as we currently envision it.
    
    In addition, we include also the start of the SQL files that will define
    and populate the actual database.
    
    One of these sets up the schema, and is intended to be executed against
    any database and ensure that the proper schema is in place, dropping any
    data and tables required to do so (unconditionally).
    
    The second bootstraps the initial data into the tables in order to get
    things up and running. Currently this is the known set of roles and also
    an initial user that represents me.

---
+++ db/bootstrap.sql
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- D A T A   B O O T S T R A P P I N G -----------------------------------------
+--------------------------------------------------------------------------------
+
+
+-- Set up a distinct list of known roles in the system.
+--   * "admin" has full site admin rights
+--   * "superuser" is as a regular user, but with some extra privileges like
+--                 having an account that does not expire.
+--   * "user" is a regular user
+-- Set up the distinct list of known roles in the system.
+INSERT INTO Roles(roleId, name, accessLevel)
+  VALUES
+    ('SctYBZqwLgiuNgZgkATsZ', 'admin',     0),
+    ('NJYiqxe2rkiFuAmu3DJDD', 'superuser', 1),
+    ('rVBBPOUWqPbWBmiUQuwGs', 'user',      2)
+  ;
+
+
+-- Insert an initial user to be the administrator. Unless you're the person that
+-- wrote this software, you probably want to change this if you run your own
+-- version, unless you'd like OdatNurd to be an administrator on your site.
+INSERT INTO Users(userId, role, acheronUser, acheronMethod, name, emailAddress, profileImage )
+  VALUES
+    ('XHmX-SwOs-r-CVfbFpDq3', 'SctYBZqwLgiuNgZgkATsZ',
+        '13212876', 'github',
+        'OdatNurd', 'odatnurd@gmail.com', 'https://avatars.githubusercontent.com/u/13212876?v=4')
+    ;
+
+
+--------------------------------------------------------------------------------
---
+++ db/schema.sql
@@ -0,0 +1,73 @@
+--------------------------------------------------------------------------------
+--- C L E A N U P --------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Roles;
+
+
+--------------------------------------------------------------------------------
+-- U S E R   I N F O R M A T I O N ---------------------------------------------
+--------------------------------------------------------------------------------
+
+-- Each user that is known to the system has a role assigned to them that
+-- outlines how different parts of the system act towards that user.
+--
+-- Each role has a numeric access level assigned to it, which is what is used to
+-- actually control the role system; the lower the value, the higher the access
+-- level.
+CREATE TABLE IF NOT EXISTS Roles (
+    roleId TEXT UNIQUE PRIMARY KEY,
+    name TEXT NOT NULL,
+
+    accessLevel INTEGER NOT NULL
+);
+
+
+
+--------------------------------------------------------------------------------
+
+
+-- Each user that is known to the system is tracked by an entry in this table.
+--
+-- Internally, each user has a unique nanoid identifier created for them at the
+-- time they are added to the table.
+--
+-- Externally, we use Acheron as our authentication back end. Acheron supports
+-- multiple authentication back ends, so the table here tracks the unique keys
+-- that are provided to us, allowing for a potential mixed authentication system
+-- to be used.
+--
+-- Currently the disambiguating factor is the Acheron information, which means
+-- that logging in via different methods can cause the same user to appear twice
+-- in the list even if their other details are the same. This is because not all
+-- supported back ends guarantee the email address will be set.
+--
+-- Runtime support could be added to merge users together if it was known they
+-- were the same user logging in via multiple methods.
+CREATE TABLE IF NOT EXISTS Users (
+    userId TEXT UNIQUE PRIMARY KEY,
+    role TEXT REFERENCES Roles(roleId),
+
+    acheronUser TEXT NOT NULL,
+    acheronMethod TEXT NOT NULL,
+
+    name TEXT NOT NULL,
+    emailAddress TEXT NOT NULL DEFAULT(''),
+    profileImage TEXT NOT NULL DEFAULT(''),
+
+    -- Default the signup and login times to whatever the current time is when
+    -- the record is created
+    signup DATETIME NOT NULL DEFAULT(strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+    lastLogin DATETIME NOT NULL DEFAULT(strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+
+    isActive INTEGER NOT NULL DEFAULT(1)
+);
+
+-- We need to be able to look up a user not only by their ID, but also by the
+-- external Acheron authentication method, so that we can cross associate the
+-- two.
+CREATE UNIQUE INDEX idx_auth_user on Users(acheronUser, acheronMethod);
+
+
+--------------------------------------------------------------------------------
