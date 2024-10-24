064ac8153ed0f979b1e93526881a81e34d895642
Terence Martin <odatnurd@gmail.com>
2024-10-23 21:45:49 -0500

    Update Acheron and how we do a logout operation
    This updates to the latest version of Acheron, which supports a logout
    route that we can bind to so that we can allow for a logout without
    knowing what the name of the cookie is instead of hard coding it like
    we did previously.

--- client/functions/logout.js
+++ client/functions/logout.js
@@ -7,7 +7,17 @@
  * This does not bother to check if there is actually a logged in user or not;
  * if that matters to you, try to get the user information first. */
 export const onRequestGet = async (ctx) => {
-  const headers = new Headers({ location: '/', 'Set-Cookie': 'obol=; Expires=Thu, 01 Jan 1970 00:00:00 UTC' });
+  // Proxy to the Acheron route to get the response that will log the user out,
+  // and then grab the cookie headers from it
+  const logout = await ctx.env.acheron.fetch(ctx.request);
+  const cookies = logout.headers.getSetCookie();
+
+  // Create new headers that redirect to the root of the site and also set all
+  // cookies as provided by the Acheron route
+  const headers = new Headers({ location: '/'});
+  cookies.forEach(cookie => headers.append('Set-Cookie', cookie));
+
+  // Redirect
   return new Response(null, { headers, status: 302 });
 }
 
